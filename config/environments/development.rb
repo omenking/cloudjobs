488c977e56ca437389380c4023b49569664dc101
Andrew Brown <andrew@monsterboxpro.com>
2024-01-27 18:07:50 +0000

    #5 configure database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "3000-omenking-cloudjobs-vvtckcyzddw.ws-us107.gitpod.io"
+  config.hosts << "3000-#{ENV['GITPOD_WORKSPACE_ID']}.#{ENV['GITPOD_WORKSPACE_CLUSTER_HOST']}"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
